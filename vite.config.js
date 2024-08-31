5d64598be85638ba974fa8f1c500322d21c7131f
Emin <99510258+emin8800@users.noreply.github.com>
2024-08-31 18:41:41 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import react from '@vitejs/plugin-react';
+
 export default {
+  plugins: [react()], // React pluginini ekleyin
   server: {
     host: true, // Veya '0.0.0.0' olarak belirtin
     port: process.env.PORT || 3000,
